4469a1111a5d2d149cf281893d87dc45532e12b3
Eric Cabrel <tericcabrel@yahoo.com>
2019-09-29 17:38:38 +0100

    Design contact list

--- src/components/ContactList/index.tsx
+++ src/components/ContactList/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Badge, ListGroupItem, ListGroup } from 'reactstrap';
+
+import './contact-list.css';
 
 const ContactList: React.FC = () => {
   return (
     <Row>
       <Col lg="12">
-        <h1>Contact List</h1>
+        <div className="contact-list">
+          <div className="contact-list-header d-flex justify-content-between align-items-center mb-4">
+            <div className="font-weight-bold">Contact list</div>
+            <Badge color="success">14</Badge>
+          </div>
+          <div className="contact-list-body">
+            <ListGroup>
+              <ListGroupItem className="d-flex flex-column">
+                <span className="name">Eric Cabrel TIOGO</span>
+                <span className="number small">693642889</span>
+              </ListGroupItem>
+            </ListGroup>
+          </div>
+        </div>
       </Col>
     </Row>
   );
